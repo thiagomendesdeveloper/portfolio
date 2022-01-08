e3b5e108a60b5eeba58d1463fcc4be1990a391f2
thiagomendesdeveloper <thiagomendessilva77@gmail.com>
2022-01-08 11:33:44 -0200

    Menu mobile

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import './style.scss'
 
+
 export default function Menu(){
+
+    const [ menu, setMenu ] = useState(false);
+    
+    const click = () => setMenu(!menu);
+
+    const menuVisible = {
+        right: menu ? 0 : '100%'
+    }
+
+    console.log(menu)
+    console.log(menuVisible)
+
     return(
+        <>
         <div className="menu">
-            <img src="" alt="" />
+            <FaBars className="nav-bar" onClick={click} />
             <div className="menu-itens">
                 <a class="light" href="#Home">Home</a>
                 <a class="light" href="#Sobre">Sobre</a>
                 <a class="light" href="#Projects">Projects</a>
             </div>
         </div>
+        <div className="menu-mobile" style={menuVisible}>
+            <h2 onClick={click}>X</h2>
+            <div className="menu-mobile-itens">
+                <a class="menu-mobile-item" href="#Home">Home</a>
+                <a class="menu-mobile-item" href="#Sobre">Sobre</a>
+                <a class="menu-mobile-item" href="#Projects">Projects</a>
+            </div>
+        </div>
+        </>
     )
 }
